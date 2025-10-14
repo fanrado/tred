eac08f0ac7bb53feb588eec4fbe6099bec8035bc
fanrado <radofana@gmail.com>
2025-10-13 20:36:22 -0400

    full sim config related files

--- tests/playground/zero_out_wf/run_zero_out_wf.sh
+++ tests/playground/zero_out_wf/run_zero_out_wf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-OutputFile="$1"
-uv run tred -c config.yaml fullsim \
-   -i /home/yousen/Public/ndlar_shared/data/MR5_2x2_single_particles/segments_pid13.hdf5 \
+# OutputFile="$1"
+OutputFile="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/MEMORY_EVAL/defaultconfig_event1004.npz"
+OutputLog="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/MEMORY_EVAL/log_graph_effq.log"
+uv run tred -c config.yaml -l $OutputLog fullsim \
+   -i /home/rrazakami/work/ND-LAr/data_tred/MicroProdN1p1_NDLAr_1E18_RHC.convert2h5.nu.0000001.EDEPSIM.hdf5 \
    -o $OutputFile

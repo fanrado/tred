1fdb9851d9bbc9223d18483509cd881c88c480e9
fanrado <radofana@gmail.com>
2025-11-11 21:59:58 -0800

    change to generate results

--- tests/playground/zero_out_wf/run_zero_out_wf.sh
+++ tests/playground/zero_out_wf/run_zero_out_wf.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # OutputFile="$1"
-OutputFile="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/10x10_partitions_2x2x2.npz"
-OutputLog="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/log_10x10_partitions_2x2x2.log"
+# subdir="cut_on_drifttime_30_100_nocut_event1003"
+# subdir="cap_on_diffspread"
+subdir="November10_2025/"
+OutputFile="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/${subdir}/10x10_4x4x2_tdrift20.npz"
+# OutputFile="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/event1004_pid211_segment233928.npz"
+# OutputFile="debug/event1004_pid211_segment233928.npz"
+OutputLog="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/${subdir}/10x10_4x4x2_tdrift20.log"
+# OutputLog="/home/rrazakami/work/ND-LAr/starting_over/OUTPUT_EVAL/ACC_EFFQ/event1004_pid211_segment233928.log"
+# OutputLog="debug/event1004_pid211_segment233928.log"
 InputFile="/home/rrazakami/work/ND-LAr/data_tred/MicroProdN1p1_NDLAr_1E18_RHC.convert2h5.nu.0000001.EDEPSIM.hdf5"
-# InputFile="/home/rrazakami/work/ND-LAr/data_tred/MR5_2x2_single_particles/segments_pid211.hdf5"
+# InputFile="/home/rrazakami/work/ND-LAr/data_tred/event1004_pid211_segment233928.hdf5"
 uv run tred -c config.yaml -l $OutputLog fullsim \
    -i $InputFile \
    -o $OutputFile

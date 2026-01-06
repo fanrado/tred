e00a4b068e67fae1901cfaa5c414b2e9fe782ad5
fanrado <radofana@gmail.com>
2026-01-06 12:28:05 -0800

    remove PYTORCH_CUDA_ALLOC_CONF from the bash script to see the difference between with and without

--- tests/playground/zero_out_wf/run_zero_out_wf.sh
+++ tests/playground/zero_out_wf/run_zero_out_wf.sh
@@ -5,6 +5,9 @@ OutputFile="/home/rrazakami/work/ND-LAr/tredBenchmark_update_Dec17_2025/OUTPUT_E
 OutputLog="/home/rrazakami/work/ND-LAr/tredBenchmark_update_Dec17_2025/OUTPUT_EVAL/MEMORY_EVAL/mem_benchmark_differentBatchsizes_event1003.log"
 InputFile="/home/rrazakami/work/ND-LAr/data_tred/MicroProdN1p1_NDLAr_1E18_RHC.convert2h5.nu.0000001.EDEPSIM.hdf5"
 # InputFile="/home/rrazakami/work/ND-LAr/data_tred/MR5_2x2_single_particles/segments_pid211.hdf5"
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True uv run tred -c config.yaml -l $OutputLog fullsim \
+# PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True uv run tred -c config.yaml -l $OutputLog fullsim \
+#    -i $InputFile \
+#    -o $OutputFile
+uv run tred -c config.yaml -l $OutputLog fullsim \
    -i $InputFile \
    -o $OutputFile

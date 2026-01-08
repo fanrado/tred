e861bf97b794a5ec71f5296d79f27e51daf02213
fanrado <radofana@gmail.com>
2026-01-08 16:30:25 -0500

    set cuda alloc conf to expandable_segments:True

--- tests/playground/zero_out_wf/run_zero_out_wf.sh
+++ tests/playground/zero_out_wf/run_zero_out_wf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # OutputFile="$1"
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 OutputFile="/home/rrazakami/work/ND-LAr/tredBenchmark_update_Dec17_2025/OUTPUT_EVAL/RUNTIME_EVAL/default_config_event1003.npz"
 OutputLog="/home/rrazakami/work/ND-LAr/tredBenchmark_update_Dec17_2025/OUTPUT_EVAL/RUNTIME_EVAL/mem_benchmark_differentBatchsizes_event1003.log"
 InputFile="/home/rrazakami/work/ND-LAr/data_tred/MicroProdN1p1_NDLAr_1E18_RHC.convert2h5.nu.0000001.EDEPSIM.hdf5"
 # InputFile="/home/rrazakami/work/ND-LAr/data_tred/MR5_2x2_single_particles/segments_pid211.hdf5"
-uv run tred -c config.yaml -l $OutputLog fullsim \
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True uv run tred -c config.yaml -l $OutputLog fullsim \
    -i $InputFile \
    -o $OutputFile
